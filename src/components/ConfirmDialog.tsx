b897b4b4e73b4a3ac6d951e8ad181c9ea300741e
ddemile <emilienhoarau10@gmail.com>
2024-03-10 22:37:39 +0400

    feat: Light theme support for ConfirmDialog

--- src/components/ConfirmDialog.tsx
+++ src/components/ConfirmDialog.tsx
@@ -1,23 +1,23 @@
 import { FaTriangleExclamation } from "react-icons/fa6";
-import Button from './modals/Button.tsx';
 import Modal from './modals/Modal.tsx';
+import { SmallModal } from "./modals/SmallModal.tsx";
 
 export default function ConfirmDialog({ onConfirm, onCancel, isOpen, title, subtitle }: { onConfirm: () => void, onCancel: () => void, isOpen: boolean, title: string, subtitle: string }) {
     return (
         <Modal isOpen={isOpen} onRequestClose={onCancel} shouldCloseOnEsc={false} shouldCloseOnOverlayClick={false}>
-            <div className="rounded-lg w-[440px] overflow-hidden mx-auto">
-                <div className="bg-[#303031] p-2 relative flex flex-col pb-4">
+            <SmallModal.Container>
+                <SmallModal.Content className="pb-4">
                     <span className=" mx-auto my-5">
-                        <FaTriangleExclamation className="text-neutral-500" size={50} />
+                        <FaTriangleExclamation className="text-neutral-300 dark:text-neutral-500" size={50} />
                     </span>
                     <p className="font-bold text-xl mt-1">{title}</p>
                     <p>{subtitle}</p>
-                </div>
-                <div className="bg-zinc-800 p-3 flex justify-end gap-2">
-                    <Button onClick={onCancel} type="discard">Cancel</Button>
-                    <button onClick={onConfirm} className="disabled:bg-red-800 disabled:text-zinc-400 disabled:cursor-not-allowed rounded-md bg-red-800 hover:bg-red-900 transition-colors duration-300 p-1 px-4">Confirm</button>
-                </div>
-            </div>
+                </SmallModal.Content>
+                <SmallModal.Footer>
+                    <SmallModal.Button onClick={onCancel} variant="discard">Cancel</SmallModal.Button>
+                    <button onClick={onConfirm} className="text-white disabled:text-zinc-400 disabled:cursor-not-allowed rounded-md bg-red-500 dark:bg-red-800 hover:bg-red-600 dark:hover:bg-red-900 transition-colors duration-300 p-1 px-4">Confirm</button>
+                </SmallModal.Footer>
+            </SmallModal.Container>
         </Modal>
     )
 }
