158191bfab0a5364277a5984c193e3f04d8cd49b
Emilien Hoarau <104416229+ddemile@users.noreply.github.com>
2025-03-08 11:25:21 +0400

    Merge release 1.4.3 (#9)
    * Release (#7)
    
    * release(1.4.1): Setup auto build
    
    * feat: test rust-cache
    
    * fix: rust cache
    
    * perf: test rust-cache performance
    
    * fix: updater endpoint
    
    * fix: update no longer blocks main thread
    
    * feat: add tray button to control window state
    
    * fix: move user data to localStorage
    
    * feat: give focus to alredy opened instance on open
    
    * fix: store auth data to file
    
    * release(1.4.3): fix url

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,1 +1,1 @@
-export const BASE_API_URL = import.meta.env.PROD ? "https://soundboard.nano3.fr/api" : "http://localhost:4444"
+export const BASE_API_URL = import.meta.env.PROD ? "https://soundboard.ddemile.me/api" : "http://localhost:4444"
